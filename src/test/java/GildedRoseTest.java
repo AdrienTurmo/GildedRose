e0da84e8baa13cbbdfcc659e7f2354447a0103e7
Adrien Turmo <adrien.turmo@gmail.com>
2017-06-30 14:48:26 +0200

    "first commit"

---
+++ src/test/java/GildedRoseTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class GildedRoseTest {
+
+    @Test
+    public void foo() {
+        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("fixme", app.items[0].name);
+    }
+
+}
